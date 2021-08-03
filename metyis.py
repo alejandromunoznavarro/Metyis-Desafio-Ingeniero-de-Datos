df15442da7c36fb826d60d7350a33133fc151cd9
Alejandro Muñoz Navarro <alejandro@munoznavarro.com>
2021-08-03 22:22:24 +0200

    Ejercicios obligatorios
    Ejercicios obligatorios completados (sin pruebas unitarias)

--- metyis.py
+++ metyis.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Aug  2 14:13:09 2021
 
-@author: alejandromunoz
+@author: Alejandro Muñoz Navarro
 """
 
 # Importamos librerias
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 import sqlite3
 from sqlite3 import Error
 
-class BBDD:
+class Metyis:
     # Definimos el rango de años
     minimo = 0
     maximo = 0
@@ -23,31 +23,34 @@ class BBDD:
     def __init__(self):
         self.minimo = 2014
         self.maximo = 2017
-        
-        try:
-            self.connect()
-            self.conectado = 1
-        except Exception:
-            self.conectado = 0
+        self.connect()
 
-    # Conectamos a la Base de Datos
+    """
+    Conexión a la base de datos
+    """
     def connect(self):
-
         try:
             self.con = sqlite3.connect('Metyis.db')
-
+            self.conectado = 1
+            pass
         except Error:
-            print("Error de conexión con la Base de Datos")
-        
+            print("\x1b[1;31m"+"Error de conexión con la Base de Datos")
+     
+    """
+    Desconexión de la base de datos
+    """
     def disconnect(self,con):
         try:
             if self.conectado == 1:
                 self.con.close()
-
-        except Error:
-            print("Error de desconexión con la Base de Datos")
+            pass
+        except Exception:
+            print("\x1b[1;31m"+"Error de desconexión con la Base de Datos")
             
-    # Creamos las tablas y añadimos los datasets
+    """
+    Creación de las tablas en la base de datos'
+    """
+    
     def create_Tables(self):
         years = range(self.minimo,self.maximo+1)
     
@@ -58,8 +61,13 @@ class BBDD:
             # Leemos OD por cada año
             df = pd.read_csv("dataset/OD_"+ str(i) +".csv")
         
-            cursorObj.execute("CREATE TABLE OD_"+str(i)+" ([Unnamed: 0] INTEGER PRIMARY KEY, [start_date] date, [start_station_code] integer, [end_date] date, [end_station_code] integer, [duration_sec] integer, [is_member] integer)")
-            df.to_sql('OD_'+str(i), self.con, if_exists='append', index = False)
+            cursorObj.execute('''CREATE TABLE OD_'''+str(i)+''' ([Unnamed: 0] 
+                              INTEGER PRIMARY KEY, [start_date] date, 
+                              [start_station_code] integer, [end_date] date, 
+                              [end_station_code] integer, [duration_sec] 
+                              integer, [is_member] integer)''')
+            df.to_sql('OD_'+str(i), self.con, 
+                      if_exists='append', index = False)
             print(df.columns)
     
             # Leemos Stations por cada año
@@ -67,22 +75,45 @@ class BBDD:
             
             
             if(i == 2017):
-                cursorObj.execute("CREATE TABLE Stations_"+str(i)+" ([code] INTEGER PRIMARY KEY, [name] text, [latitude] integer, [longitude] integer, [is_public] integer)")
+                cursorObj.execute('''CREATE TABLE Stations_'''+str(i)+''' 
+                                  ([code] INTEGER PRIMARY KEY, [name] text, 
+                                   [latitude] integer, [longitude] integer, 
+                                   [is_public] integer)''')
             else:
-                cursorObj.execute("CREATE TABLE Stations_"+str(i)+" ([code] INTEGER PRIMARY KEY, [name] text, [latitude] integer, [longitude] integer)")
-            df.to_sql('Stations_'+str(i), self.con, if_exists='append', index = False)
+                cursorObj.execute('''CREATE TABLE Stations_'''+str(i)+''' 
+                                  ([code] INTEGER PRIMARY KEY, [name] text, 
+                                   [latitude] integer, [longitude] integer)''')
+            df.to_sql('Stations_'+str(i), self.con, 
+                      if_exists='append', index = False)
             print(df.columns)
     
         self.con.commit()
     
 
-    '''
+    def combinar(self, row1, row2, N:int):
+        # Combinamos ambas listas
+        aux = [[i[0],i[1]] for i in row1]
+        for i in row2:
+            encontrado = 0
+            for j in aux:
+                if i[0] == j[0] and not encontrado:
+                    encontrado = 1
+                    j[1] +=i [1]
+            if encontrado == 0:
+                aux.append(i)
+          
+        # Ordenamos la lista y devolvemos el top N
+        aux.sort(key = lambda x: x[1], reverse=True)
+        lista_combinada = aux[0:N]
+        return lista_combinada
+    
+    """
     EJERCICIOS
-    '''
+    """
 
-    ''' 
-    1. Histograma de tiempos de viaje para un año dado
-    '''
+    """ 
+    1.  Histograma de tiempos de viaje para un año dado
+    """
     def get_hist(self, year:int):
         try:
             cursorObj = self.con.cursor()
@@ -101,59 +132,151 @@ class BBDD:
             plt.xlabel('Tiempos')
             plt.ylabel('Frecuencia')
             # Dibujamos el histograma
-            plt.show()     
+            plt.show()  
+            pass
 
         except Exception:
             print("\x1b[1;31m"+"ERROR! Año no encontrado")
     
-    '''
-    2. Listado del Top N de estaciones más utilizadas para un año dado.
+    """
+    2.  Listado del Top N de estaciones más utilizadas para un año dado.
         Dividirlo en:
         1. Estaciones de salida
         2. Estaciones de llegada
         3. En general
-    '''
+    """
     def get_TopN(self, N:int, year:int, option:int):
-        
+        if not type(N) == int or N <= 0:
+            raise ValueError('Valor de N incorrecto. Debe ser un número positivo')
+        elif not type(year) == int or year < self.minimo or year > self.maximo:
+            raise ValueError('Valor del año incorrecto. Debe ser un número entre '
+                             + str(self.minimo)+' y '+ str(self.maximo))
+        elif not type(option) == int or option < 1 or option > 3:
+            raise ValueError('Opción incorrecta. Seleccione 1, 2 o 3')
         try:
             cursorObj = self.con.cursor()
 
-            if option == 1:
-                cursorObj.execute("SELECT name, start_station_code, count(start_station_code) AS total FROM Stations_"+str(year)+", OD_"+str(year)+" WHERE code = start_station_code GROUP BY start_station_code ORDER BY total DESC LIMIT "+str(N))
+            if option == 3:
+                # Caso 3 - SIMPLIFICAR
+                cursorObj.execute('''SELECT st.name, 
+                                  count(od.start_station_code) AS total 
+                                  FROM OD_'''+str(year)+''' od 
+                                  INNER JOIN Stations_'''+str(year)+''' st 
+                                  ON od.start_station_code = st.code 
+                                  GROUP BY st.code''')
+                row1 = cursorObj.fetchall()
                 
-            elif option == 2:
-                cursorObj.execute("SELECT name, start_station_code, count(end_station_code) AS total FROM Stations_"+str(year)+", OD_"+str(year)+" WHERE code = end_station_code GROUP BY end_station_code ORDER BY total DESC LIMIT "+str(N))
-               
+                cursorObj.execute('''SELECT st.name, count(od.end_station_code)
+                                  AS total FROM OD_'''+str(year)+''' od 
+                                  INNER JOIN Stations_'''+str(year)+''' st 
+                                  ON od.end_station_code = st.code 
+                                  GROUP BY st.code ''')
+                row2 = cursorObj.fetchall()
+                rows = self.combinar(row1,row2,N)
+                
+                
             else:
-                cursorObj.execute("SELECT name, start_station_code, count(start_station_code) AS total FROM Stations_"+str(year)+", OD_"+str(year)+" WHERE code = start_station_code GROUP BY start_station_code ORDER BY total DESC LIMIT "+str(N))
+                if option == 2:
+                    # Caso 2
+                    cursorObj.execute('''SELECT st.name, 
+                                      count(od.end_station_code) AS total 
+                                      FROM OD_'''+str(year)+''' od 
+                                      INNER JOIN Stations_'''+str(year)+''' st 
+                                      ON od.end_station_code = st.code 
+                                      GROUP BY st.code ORDER BY total DESC 
+                                      LIMIT '''+str(N))
                
-            rows = cursorObj.fetchall()
+                else:
+                    # Caso 1
+                    cursorObj.execute('''SELECT st.name, 
+                                      count(od.start_station_code) AS total 
+                                      FROM OD_'''+str(year)+''' od 
+                                      INNER JOIN Stations_'''+str(year)+''' st 
+                                      ON od.start_station_code = st.code 
+                                      GROUP BY st.code ORDER BY total DESC 
+                                      LIMIT '''+str(N))
+                
+               
+                rows = cursorObj.fetchall()
             stations = [i[0] for i in rows]
-            print(stations)
-            '''
-            tiempos = [i[0] for i in rows]
-        
-            # Calculamos los extremos de los intervalos
-            intervalos = range(min(tiempos), max(tiempos) + 2) 
-
-            plt.hist(x=tiempos, bins=intervalos, color='#F2AB6D')
-            plt.title('Histograma de tiempos en '+str(year))
-            plt.xlabel('Tiempos')
-            plt.ylabel('Frecuencia')
-            # Dibujamos el histograma
-            plt.show()     
-            '''
-        except Exception:
+            for i in range(0,N):
+                print(i+1,stations[i])
+            pass
+        except Error:
             print("\x1b[1;31m"+"ERROR! Año no encontrado")
         
 
+    """
+    3.  Listado del Top N de viajes más comunes para un año dado. 
+        Donde un viaje se define por su estación de salida y de llegada
+    """
+    def get_TopNViajes(self, N:int, year:int):
+        if not type(N) == int or N <= 0:
+            raise ValueError('Valor de N incorrecto. Debe ser un número positivo')
+        elif not type(year) == int or year < self.minimo or year > self.maximo:
+            raise ValueError('Valor del año incorrecto. Debe ser un número entre '
+                             + str(self.minimo)+' y '+ str(self.maximo))
 
-# Listado del Top N de viajes más comunes para un año dado. Donde un viaje se define por su estación de salida y de llegada
-
-# Identificación de horas punta para un año determinado sin tener en cuenta el día. Es decir, si es día de semana, fin de semana, festivo o temporada del año.
-
-user = BBDD()
-#user.create_Tables()
-user.get_hist(2016)
-user.get_TopN(5, 2014, 2)
-
+        try:
+            cursorObj = self.con.cursor()
+            cursorObj.execute('''SELECT st1.name AS name1, st2.name AS name2,
+                              od.start_station_code, od.end_station_code, 
+                              count(*) AS total FROM OD_'''+str(year)+'''
+                              od INNER JOIN Stations_'''+str(year)+'''
+                              st1, Stations_'''+str(year)+''' st2 
+                              ON st1.code = od.start_station_code AND 
+                              st2.code = od.end_station_code GROUP BY 
+                              od.start_station_code, od.end_station_code 
+                              ORDER BY total DESC LIMIT '''+str(N))
+                
+               
+            rows = cursorObj.fetchall()
+            stations = [[i[0],i[1]] for i in rows]
+                
+            for i in range(0,N):
+                print(i+1,'Salida:',stations[i][0], 
+                      '\n\tLlegada:',stations[i][1])
+            pass
+        except Exception:
+            print("\x1b[1;31m"+"ERROR! Año no encontrado")
+    
+    """
+    4.  Identificación de horas punta para un año determinado sin tener en 
+        cuenta el día.
+        Es decir, si es día de semana, fin de semana, festivo o temporada del 
+        año.
+    """
+    def get_NHoraPunta(self,N:int,year:int):
+        if not type(N) == int or N <= 0:
+            raise ValueError('Valor de N incorrecto. Debe ser un número positivo')
+        elif not type(year) == int or year < self.minimo or year > self.maximo:
+            raise ValueError('Valor del año incorrecto. Debe ser un número entre '
+                             + str(self.minimo)+' y '+ str(self.maximo))
+            
+        try:
+            cursorObj = self.con.cursor()
+            cursorObj.execute('''SELECT time(o1.start_date), count(*) AS total
+                              FROM OD_'''+str(year)+''' o1 
+                              GROUP BY time(o1.start_date)''') 
+            salidas = cursorObj.fetchall()
+            cursorObj.execute('''SELECT time(o1.end_date), count(*) AS total
+                              FROM OD_'''+str(year)+''' o1 
+                              GROUP BY time(o1.end_date)''')
+            llegadas = cursorObj.fetchall()
+                
+            hora = self.combinar(salidas,llegadas,N)
+             
+            for i in range(0,N):
+                print(i+1,'Hora:',hora[i][0])
+            pass
+        except Exception:
+            print("\x1b[1;31m"+"ERROR! Año no encontrado")
+            
+    
+if __name__ == "__main__":  
+    user = Metyis()
+    #user.create_Tables()
+    #user.get_hist(2016)
+    user.get_TopN(2, 2014, 4)
+    #user.get_TopNViajes(6, 2014)
+    #user.get_NHoraPunta(6, 2014)
